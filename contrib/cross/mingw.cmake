d3c88823e1b58da30146ae4826d7960ac8a1d3a0
despair86 <despair@rvx86.net>
2018-09-26 01:55:51 -0500

    oh right

--- contrib/cross/mingw.cmake
+++ contrib/cross/mingw.cmake
@@ -1,15 +1,35 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
-# cross compilers to use
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+add_definitions("-DWINNT_CROSS_COMPILE")
 
 # target environment on the build host system
-set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
+# second one is for non-root installs
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} /home/$ENV{USER}/mingw32 /home/$ENV{USER}/mingw32/${TOOLCHAIN_PREFIX})
 
 # modify default behavior of FIND_XXX() commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# cross compilers to use
+if($ENV{COMPILER} MATCHES "clang")
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-clang)
+set(CMAKE_CXX_COMPILER_AR llvm-ar)
+set(CMAKE_C_COMPILER_AR ${CMAKE_CXX_COMPILER_AR})
+set(CMAKE_CXX_COMPILER_RANLIB llvm-ranlib)
+set(CMAKE_C_COMPILER_RANLIB ${CMAKE_CXX_COMPILER_RANLIB})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-clang++)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-command-line-argument")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-command-line-argument -Wno-c++11-narrowing")
+# because clang is insane enough to inline whole sections of the C++ library!
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--allow-multiple-definition")
+else()
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+endif()
+
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+set(WOW64_CROSS_COMPILE ON)
+set(CMAKE_C_STANDARD_LIBRARIES ${CMAKE_C_STANDARD_LIBRARIES} ws2_32)
+set(CMAKE_CXX_STANDARD_LIBRARIES ${CMAKE_CXX_STANDARD_LIBRARIES} ws2_32)
